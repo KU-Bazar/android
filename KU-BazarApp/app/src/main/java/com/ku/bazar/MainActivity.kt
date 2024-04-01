20ce56f6e65dd9d14a6beb7c1d7fe44a3c8d736e
Neer-rn <neerrnbhatta@gmail.com>
2024-04-01 22:08:48 +0545

    feat: Managing the configuration

--- KU-BazarApp/app/src/main/java/com/ku/bazar/MainActivity.kt
+++ KU-BazarApp/app/src/main/java/com/ku/bazar/MainActivity.kt
@@ -1,13 +1,43 @@
 package com.ku.bazar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ku.bazar.ui.theme.KUBazarTheme
+
 //import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {  }
+        setContent {
+            KUBazarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KUBazarTheme {
+        Greeting("Android")
     }
 }
