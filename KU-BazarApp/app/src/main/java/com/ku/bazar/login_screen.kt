4dbb80e064084b3b20b44b8be115e68a0c39e3cc
Neer-rn <neerrnbhatta@gmail.com>
2024-05-13 18:10:43 +0545

    feat: Removed the action bar from .MainActivity

--- KU-BazarApp/app/src/main/java/com/ku/bazar/login_screen.kt
+++ KU-BazarApp/app/src/main/java/com/ku/bazar/login_screen.kt
@@ -1,6 +1,5 @@
 package com.ku.bazar
 
-import android.icu.text.ListFormatter.Width
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -8,14 +7,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import com.ku.bazar.util.login
 
 @Composable
@@ -23,31 +19,46 @@ import com.ku.bazar.util.login
 fun login(){
     Box(
         modifier = Modifier.fillMaxSize()
-    ){
-        Column(modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top
         ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
                 Image(
-                    modifier = Modifier
+                    modifier =Modifier
                         .fillMaxWidth()
-                        .height(0.35f.dp),
+                        .aspectRatio(1f),
                     painter = painterResource(
                         id = login.first.first_background
                     ),
                     contentDescription = "@string/wall_des"
                 )
             }
+
+            Box(
+                modifier = Modifier
+                    .weight(2f)
+            ) {
                 Canvas(
                     modifier = Modifier
+                        .fillMaxSize()
                         .fillMaxWidth()
-                        .height(0.65f.dp)
                         .align(Alignment.BottomCenter)
-                        .offset(y = (-100).dp)
-                ){
+                        .aspectRatio(1f)
+
+                ) {
                     drawRoundRect(
                         color = Color.White,
-                        cornerRadius = CornerRadius(100f,100f)
+                        size = Size(size.maxDimension, size.maxDimension),
+                        cornerRadius = CornerRadius(10f, 10f)
                     )
                 }
             }
-
         }
+    }
+}
